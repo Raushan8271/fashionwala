082cc969e6a24dd9de60b1804516e9936e4ffdac
Raushan8271 <raushan8271@gmail.com>
2021-04-05 09:07:14 +0530

    fashionwala.com

--- js/otp.js
+++ js/otp.js
@@ -2,6 +2,7 @@ function otp() {
     var otp = document.getElementById('otpno').value;
     if (otp == "123456") {
         alert('Payment Successful')
+        localStorage.removeItem('cart');
     } else {
         alert('Incorrect OTP')
     }
